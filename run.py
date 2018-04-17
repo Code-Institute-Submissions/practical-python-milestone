a044551f53525f42f4d7051d3383ad82d483cb32
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-17 17:26:10 +0000

    Added winners page

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ import json
 from flask import Flask, redirect, render_template, request, flash
 
 app = Flask(__name__)
-
-username = ""
 counter = 0
 user_answer=""
 data= []
@@ -19,29 +17,40 @@ def write_to_file(filename, data):
 
 
 @app.route('/', methods=["GET", "POST"])
-def index():
+def index(username):
     """Main page with instructions"""
-    global username
     # Handle POST request
     if request.method == "POST":
         write_to_file("data/users.txt", request.form["username"] + "\n")
-        return redirect(request.form["username"])
+        return redirect("/riddles")
     return render_template("index.html", username=username)
     
-@app.route('/<username>', methods=["GET", "POST"])
+@app.route('/riddles', methods=["GET", "POST"])
 def next_riddle(username):
     global counter
     global data
     global user_answer
     
-    if request.method == "POST":
-        if data[counter]["answer"] == request.form["answer"]:
-            counter += 1
-            user_answer = ""
-        else:
-            user_answer = request.form["answer"] + " is incorrect!"
+    if len(data) == counter + 1:
+        return redirect("/congratulations")
+    else:
+        if request.method == "POST":
+            if data[counter]["answer"] == request.form["answer"]:
+                counter += 1
+                user_answer = ""
+            else:
+                user_answer = request.form["answer"] + " is incorrect!"
             
     return render_template("riddles.html", data=data, i=counter, user_answer=user_answer)  
+    
+@app.route('/congratulations', methods=["GET", "POST"])
+def congratulations(username):
+    """Main page with instructions"""
+    write_to_file("data/winners.txt", username+ "\n")
+    # Handle POST request
+    if request.method == "POST":
+        return redirect("/")
+    return render_template("win.html", username=username)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
