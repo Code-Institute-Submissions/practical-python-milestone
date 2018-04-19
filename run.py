eefb89f03e22f46c01c00d4bff02e0743335db4c
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-19 18:17:24 +0000

    Converted global variables to global dictionaries and changed url paths so diffenrent users can play their own instances

--- run.py
+++ run.py
@@ -4,11 +4,11 @@ import json
 from flask import Flask, redirect, render_template, request, flash
 
 app = Flask(__name__)
-counter = 0
-username = ""
 user_answer=""
-score=0
 data= []
+score_dictinary = {}
+counter_dictinary = {}
+
 with open("data/riddles.json", "r") as json_data:
         data = json.load(json_data)
         
@@ -16,68 +16,68 @@ def write_to_file(filename, data):
     """Handle the process of writing data to a file"""
     with open(filename, "a") as file:
         file.writelines(data)
+        
+    
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    global counter
     global data
-    global username
+    global score_dictinary
+    global counter_dictinary
     """Main page with instructions"""
     # Handle POST request
     if request.method == "POST":
         username = request.form["username"]
-        write_to_file("data/users.txt", username + "\n")
-        return redirect("/riddles")
+        score_dictinary[username] = 0
+        counter_dictinary[username] = 0
+        return redirect("/riddles/" + username)
     return render_template("index.html")
     
-@app.route('/riddles', methods=["GET", "POST"])
-def next_riddle():
-    global counter
+@app.route('/riddles/<username>', methods=["GET", "POST"])
+def next_riddle(username):
     global data
     global user_answer
-    global score
+    global score_dictinary
+    global counter_dictinary
     
-    if len(data) == counter + 1:
-        return redirect("/congratulations")
+    if len(data) == counter_dictinary[username] + 1:
+        return redirect("/congratulations/" + username)
     else:
         if request.method == "POST":
-            if data[counter]["answer"] == request.form["answer"]:
-                counter += 1
-                score += 1
+            if data[counter_dictinary[username]]["answer"] == request.form["answer"]:
+                score_dictinary[username] += 1
+                counter_dictinary[username] += 1
                 user_answer = ""
                 
             else:
                 user_answer = request.form["answer"] + " is incorrect!"
-                if score > 0:
-                    score -= 1
+                if score_dictinary[username] > 0:
+                    score_dictinary[username] -= 1
                     
             
-    return render_template("riddles.html", data=data, i=counter, user_answer=user_answer)  
+    return render_template("riddles.html", data=data, i=counter_dictinary[username], user_answer=user_answer, score = score_dictinary[username])  
     
-@app.route('/congratulations', methods=["GET", "POST"])
-def congratulations():
-    global username
-    global counter
-    global score
+@app.route('/congratulations/<username>', methods=["GET", "POST"])
+def congratulations(username):
     """Main page with instructions"""
-    write_to_file("data/winners.txt", username + "," + str(score) + "\n")
+    global score_dictinary
+    global counter_dictinary
     # Handle POST request
     if request.method == "POST":
-        counter = 0
-        score=0
+        write_to_file("data/winners.txt", username + " " + str(score_dictinary[username]) + "\n")
+        counter_dictinary[username] = 0
+        score_dictinary[username] = 0
         return redirect("/")
     return render_template("win.html", username=username)
     
 @app.route('/leaderboard')
 def leaderboard():
-    global username
-    global score
+    global score_dictinary
     data = []
    
     with open("data/winners.txt", "r") as file:
-        for line in file:
-            data = line
+        data = file.readlines()
     return render_template("leaderboard.html", data=data)
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
