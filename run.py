0df45e1c64418a3af592e7c3406e9e454d8432a5
Ben Hasselgren <ben.nadalito@gmail.com>
2018-04-20 10:03:13 +0000

    Added quicksort to sort leaderboard. Winners now display sorted on learboard page

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 import json
+import csv
 from flask import Flask, redirect, render_template, request, flash
 
 app = Flask(__name__)
@@ -65,7 +66,7 @@ def congratulations(username):
     global counter_dictinary
     # Handle POST request
     if request.method == "POST":
-        write_to_file("data/winners.txt", username + " " + str(score_dictinary[username]) + "\n")
+        write_to_file("data/winners.csv", username + "," + str(score_dictinary[username]) + "\n")
         counter_dictinary[username] = 0
         score_dictinary[username] = 0
         return redirect("/")
@@ -75,9 +76,59 @@ def congratulations(username):
 def leaderboard():
     global score_dictinary
     data = []
-   
-    with open("data/winners.txt", "r") as file:
-        data = file.readlines()
-    return render_template("leaderboard.html", data=data)
+    fields = []
+    scores = []
+    names = []
+    users = []
+    
+    ifile = open("data/winners.csv", "rU")
+    reader = csv.reader(ifile, delimiter=",")
+    
+    for row in reader:
+        names.append(row[0])
+        scores.append(row[1])
+    
+    ifile.close()
+    quicksort(scores, names, 0, len(scores) - 1)
+    
+    i=0
+    while i < len(scores):
+        users.append(names[i] + " " + scores[i])
+        i += 1
+    
+    return render_template("leaderboard.html", users=users)
+
+
+
+    
+    
+
+    
+def swap_values(lst, val1, val2):
+    lst[val1], lst[val2] = lst[val2], lst[val1]
+
+def quicksort(array, array2, start, end):
+
+    if start < end:
+
+        partition_index = partition(array, array2, start, end) #
+        quicksort(array, array2, start, partition_index - 1)
+        quicksort(array, array2, partition_index + 1, end)
+
+def partition(array, array2, start, end):
+
+    pivot = end
+    partition_index = start
+
+    for i in range(start, end):
+
+        if array[i] < array[pivot]:
+            swap_values(array, partition_index, i)
+            swap_values(array2, partition_index, i)
+            partition_index += 1
+
+    array[pivot], array[partition_index] = array[partition_index], array[pivot]
+    array2[pivot], array2[partition_index] = array2[partition_index], array2[pivot]
+    return partition_index
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
